e1d0cf2a2039dc640250d907d52b4e35d48fadb1
MinxZ <z670172581@icloud.com>
2018-05-22 22:52:45 +0900

    10

--- image.py
+++ image.py
@@ -22,6 +22,53 @@ from scipy import linalg
 from six.moves import range
 
 
+def resizeAndPad(img, size, padColor=255):
+
+    h, w = img.shape[:2]
+    sh, sw = size
+
+    # interpolation method
+    if h > sh or w > sw:  # shrinking image
+        interp = cv2.INTER_AREA
+    else:  # stretching image
+        interp = cv2.INTER_CUBIC
+
+    # aspect ratio of image
+    # if on Python 2, you might need to cast as a float: float(w)/h
+    aspect = w / h
+
+    # compute scaling and pad sizing
+    if aspect > 1:  # horizontal image
+        new_w = sw
+        new_h = np.round(new_w / aspect).astype(int)
+        pad_vert = (sh - new_h) / 2
+        pad_top, pad_bot = np.floor(pad_vert).astype(
+            int), np.ceil(pad_vert).astype(int)
+        pad_left, pad_right = 0, 0
+    elif aspect < 1:  # vertical image
+        new_h = sh
+        new_w = np.round(new_h * aspect).astype(int)
+        pad_horz = (sw - new_w) / 2
+        pad_left, pad_right = np.floor(pad_horz).astype(
+            int), np.ceil(pad_horz).astype(int)
+        pad_top, pad_bot = 0, 0
+    else:  # square image
+        new_h, new_w = sh, sw
+        pad_left, pad_right, pad_top, pad_bot = 0, 0, 0, 0
+
+    # set pad color
+    # color image but only one color provided
+    if len(img.shape) is 3 and not isinstance(padColor, (list, tuple, np.ndarray)):
+        padColor = [padColor] * 3
+
+    # scale and pad
+    scaled_img = cv2.resize(img, (new_w, new_h), interpolation=interp)
+    scaled_img = cv2.copyMakeBorder(
+        scaled_img, pad_top, pad_bot, pad_left, pad_right, borderType=cv2.BORDER_CONSTANT, value=padColor)
+
+    return scaled_img
+
+
 def random_rotation(x, rg, row_axis=1, col_axis=2, channel_axis=0,
                     fill_mode='nearest', cval=0.):
     """Performs a random rotation of a Numpy image tensor.
@@ -917,52 +964,6 @@ class NumpyArrayIterator(Iterator):
                                                  batch_size,
                                                  shuffle,
                                                  seed)
-
-    def resizeAndPad(img, size, padColor=255):
-
-        h, w = img.shape[:2]
-        sh, sw = size
-
-        # interpolation method
-        if h > sh or w > sw:  # shrinking image
-            interp = cv2.INTER_AREA
-        else:  # stretching image
-            interp = cv2.INTER_CUBIC
-
-        # aspect ratio of image
-        # if on Python 2, you might need to cast as a float: float(w)/h
-        aspect = w / h
-
-        # compute scaling and pad sizing
-        if aspect > 1:  # horizontal image
-            new_w = sw
-            new_h = np.round(new_w / aspect).astype(int)
-            pad_vert = (sh - new_h) / 2
-            pad_top, pad_bot = np.floor(pad_vert).astype(
-                int), np.ceil(pad_vert).astype(int)
-            pad_left, pad_right = 0, 0
-        elif aspect < 1:  # vertical image
-            new_h = sh
-            new_w = np.round(new_h * aspect).astype(int)
-            pad_horz = (sw - new_w) / 2
-            pad_left, pad_right = np.floor(pad_horz).astype(
-                int), np.ceil(pad_horz).astype(int)
-            pad_top, pad_bot = 0, 0
-        else:  # square image
-            new_h, new_w = sh, sw
-            pad_left, pad_right, pad_top, pad_bot = 0, 0, 0, 0
-
-        # set pad color
-        # color image but only one color provided
-        if len(img.shape) is 3 and not isinstance(padColor, (list, tuple, np.ndarray)):
-            padColor = [padColor] * 3
-
-        # scale and pad
-        scaled_img = cv2.resize(img, (new_w, new_h), interpolation=interp)
-        scaled_img = cv2.copyMakeBorder(
-            scaled_img, pad_top, pad_bot, pad_left, pad_right, borderType=cv2.BORDER_CONSTANT, value=padColor)
-
-        return scaled_img
 
     def _get_batches_of_transformed_samples(self, index_array):
         batch_x = np.zeros((len(index_array), self.width,
